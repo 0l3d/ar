ba0d901a25fb151874dcb13449d1d872abd3384a
oled <oled.dev+commit@gmail.com>
2026-03-04 21:28:24 +0300

    format

--- ar.c
+++ ar.c
@@ -20,7 +20,7 @@
 					 "-l	compress with lz4 or decompress on extract (.lz4)\n" \
 					 "-n	*Navigate inside of archive with unix-like commands\n\n" \
 					 "For compressed files, specify the algorithm.\n" \
-					 "For example for extract: ar -ex test.arx.xz OR ar -eg test.arx.gz\nFor example for create: ar -xc files test.arx # out: test.arx.xz \n"  
+					 "For example for extract: ar -ex test.arx.xz OR ar -eg test.arx.gz\nFor example for create: ar -xc files test.arx # out: test.arx.xz \n"
 
 #define FS_HELP "AR archive manager for .ARX\n" \
 				"For CLI: -h\n" \
@@ -78,13 +78,12 @@ folder_file_handler(LightFS * fs, char *folderpath, int parent_offset)
 		perror("opendir");
 		return;
 	}
-	
-	char old_dir[PATH_MAX];
+	char 		old_dir  [PATH_MAX];
 	if (!getcwd(old_dir, sizeof(old_dir))) {
 		perror("getcwd olddir");
 		return;
 	}
-	int curr_off = fs->movement_parent;
+	int 		curr_off = fs->movement_parent;
 	lfs_newdir(fs, folderpath, fs->movement_parent);
 	lfs_cd(fs, folderpath);
 	chdir(folderpath);
@@ -106,7 +105,7 @@ folder_file_handler(LightFS * fs, char *folderpath, int parent_offset)
 	closedir(dirp);
 }
 
-void 
+void
 file_extracter(LightFS * fs, char *name, int parent_offset)
 {
 	char           *out;
@@ -118,9 +117,8 @@ file_extracter(LightFS * fs, char *name, int parent_offset)
 		perror("file extracter fopen error");
 		return;
 	}
-	
 	printf("extracting %s, size = %lu\n", name, size);
-	
+
 	fwrite(out, 1, size, file);
 
 	fclose(file);
@@ -130,18 +128,17 @@ file_extracter(LightFS * fs, char *name, int parent_offset)
 }
 
 
-void 
+void
 fs_file_folder_handler(LightFS * fs, char *name, int parent_offset)
 {
-	int curr_off = fs->movement_parent;
-	 	int 	dofset = lfs_doffset(fs, name, parent_offset);
-	 		int fofset = lfs_foffset(fs, name, parent_offset);
-	char old_dir[PATH_MAX];
+	int 		curr_off = fs->movement_parent;
+	int 		dofset = lfs_doffset(fs, name, parent_offset);
+	int 		fofset = lfs_foffset(fs, name, parent_offset);
+	char 		old_dir  [PATH_MAX];
 	if (!getcwd(old_dir, sizeof(old_dir))) {
 		perror("getcwd olddir");
 		return;
 	}
-
 	if (dofset != -1) {
 		mkdir(name, 0755);
 		lfs_cd(fs, name);
@@ -151,7 +148,8 @@ fs_file_folder_handler(LightFS * fs, char *name, int parent_offset)
 	lfs_list(fs, &f);
 	for (int i = 0; i < f.filescount; i++) {
 		file_extracter(fs, f.file[i]->name, fs->movement_parent);
-		if (fofset != -1) break;
+		if (fofset != -1)
+			break;
 	}
 	for (int i = 0; i < f.folderscount; i++) {
 		if (fofset != -1)
@@ -163,7 +161,9 @@ fs_file_folder_handler(LightFS * fs, char *name, int parent_offset)
 	chdir(old_dir);
 }
 
-void cli_extracter(LightFS *fs, int parent_offset) {
+void 
+cli_extracter(LightFS * fs, int parent_offset)
+{
 
 	ListFF 		f;
 	lfs_list(fs, &f);
@@ -180,14 +180,13 @@ int
 main(int argc, char **argv)
 {
 	LightFS 	fs;
-	
+
 	if (argc < 1) {
 		fprintf(stderr, "AR Needs arguments, -h for help.");
 		return 1;
 	}
-	
-	int ch, nav = 0, cre = 0, exr = 0, gzip = 0, xz = 0, lz4 = 0;
-	char *cre_folders = NULL;
+	int 		ch       , nav = 0, cre = 0, exr = 0, gzip = 0, xz = 0, lz4 = 0;
+	char           *cre_folders = NULL;
 
 	while ((ch = getopt(argc, argv, "vhgxlenc:")) != -1) {
 		switch (ch) {
@@ -206,7 +205,7 @@ main(int argc, char **argv)
 			gzip = 1;
 			break;
 		case 'x':
-			xz = 1; 
+			xz = 1;
 			break;
 		case 'l':
 			lz4 = 1;
@@ -223,56 +222,51 @@ main(int argc, char **argv)
 			break;
 		}
 	}
-	
+
 	if (optind >= argc) {
 		fprintf(stderr, "-h for help.");
 		return 1;
 	}
-	
-	char *file = argv[optind];
+	char           *file = argv[optind];
 
 	fs.movement_parent = 0;
 	fs.old_parent = 0;
 
-	
+
 	if (!file) {
 		printf("-h for help.\n");
 		return 1;
 	}
 	if (exr == 1 || nav == 1) {
-	if (strstr(file, "xz") || strstr(file, "gz") || strstr(file, "lz4")) {
-		if (gzip == 1) {
-			int size = strlen("gzip ") + strlen(" -d ") + strlen(file);
-			char *full_command = malloc(size);
-			snprintf(full_command,  size, "gzip -d %s", file);
-			system(full_command);
-			free(full_command);
-		} 
-		else if (xz == 1) {
-			int size = strlen("xz ") + strlen(" -d ") + strlen(file);
-			char *full_command = malloc(size);
-			snprintf(full_command,  size, "xz -d %s", file);
-			system(full_command);
-			free(full_command);
+		if (strstr(file, "xz") || strstr(file, "gz") || strstr(file, "lz4")) {
+			if (gzip == 1) {
+				int 		size = strlen("gzip ") + strlen(" -d ") + strlen(file);
+				char           *full_command = malloc(size);
+				snprintf(full_command, size, "gzip -d %s", file);
+				system(full_command);
+				free(full_command);
+			} else if (xz == 1) {
+				int 		size = strlen("xz ") + strlen(" -d ") + strlen(file);
+				char           *full_command = malloc(size);
+				snprintf(full_command, size, "xz -d %s", file);
+				system(full_command);
+				free(full_command);
 
-		} 
-		else if (lz4 == 1) {
-			int size = strlen("lz4 ") + strlen(" -d ") + strlen(file);
-			char *full_command = malloc(size);
-			snprintf(full_command,  size, "lz4 -d %s", file);
-			system(full_command);
-			free(full_command);
+			} else if (lz4 == 1) {
+				int 		size = strlen("lz4 ") + strlen(" -d ") + strlen(file);
+				char           *full_command = malloc(size);
+				snprintf(full_command, size, "lz4 -d %s", file);
+				system(full_command);
+				free(full_command);
 
-		}
+			}
+			char           *last = strrchr(file, '.');
 
-				char *last = strrchr(file, '.');
-
-		if (last != NULL) {
-			*last = '\0';
+			if (last != NULL) {
+				*last = '\0';
+			}
 		}
 	}
-	}
-
 	fs.img = fopen(file, "r+b");
 	if (!fs.img) {
 		fs.img = fopen(file, "w+b");
@@ -281,143 +275,140 @@ main(int argc, char **argv)
 			return 1;
 		}
 	}
-
 	if (nav == 1 && exr != 1 && cre != 1) {
 		printf("type help for help\n");
-	int 		loop = 1;
-	while (loop) {
-		char 		pwdout   [256];
-		lfs_pwd(&fs, pwdout);
-		printf("/%s -> ", pwdout);
+		int 		loop = 1;
+		while (loop) {
+			char 		pwdout   [256];
+			lfs_pwd(&fs, pwdout);
+			printf("/%s -> ", pwdout);
 
-		char 		input    [512];
-		if (fgets(input, sizeof(input), stdin) == NULL) {
-			continue;
-		}
-		input[strcspn(input, "\n")] = 0;
+			char 		input    [512];
+			if (fgets(input, sizeof(input), stdin) == NULL) {
+				continue;
+			}
+			input[strcspn(input, "\n")] = 0;
 
-		char           *command = strtok(input, " ");
-		if (!command)
-			continue;
+			char           *command = strtok(input, " ");
+			if (!command)
+				continue;
 
-		if (strcmp(command, "ls") == 0) {
-			ListFF 		f;
-			lfs_list(&fs, &f);
-			for (int i = 0; i < f.entrycount; i++) {
-				if (f.entry[i]->type == TYPEDIR) {
-					printf("[DIR] %s\n", f.entry[i]->name);
-				} else {
-					printf("[FILE] %s\n", f.entry[i]->name);
+			if (strcmp(command, "ls") == 0) {
+				ListFF 		f;
+				lfs_list(&fs, &f);
+				for (int i = 0; i < f.entrycount; i++) {
+					if (f.entry[i]->type == TYPEDIR) {
+						printf("[DIR] %s\n", f.entry[i]->name);
+					} else {
+						printf("[FILE] %s\n", f.entry[i]->name);
+					}
 				}
-			}
-			lfs_free_list(&f);
-		} else if (strcmp(command, "help") == 0) {
-			printf("%s\n", FS_HELP);
-		} else if (strcmp(command, "version") == 0) {
-			printf("ar version is %s\n", VERSION_INFO);
-		} else if (strcmp(command, "addfile") == 0) {
+				lfs_free_list(&f);
+			} else if (strcmp(command, "help") == 0) {
+				printf("%s\n", FS_HELP);
+			} else if (strcmp(command, "version") == 0) {
+				printf("ar version is %s\n", VERSION_INFO);
+			} else if (strcmp(command, "addfile") == 0) {
 
-			char           *token = strtok(NULL, " ");
-			if (!token) {
-				fprintf(stderr, "missing filename\n");
-				return 1;
-			}
-			char           *file_name = strdup(token);
-			if (!file_name) {
-				perror("strdup");
-				return 1;
-			}
-			char           *folderpath = NULL;
-			token = strtok(NULL, " ");
-			if (token) {
-				folderpath = strdup(token);
-				if (!folderpath) {
-					perror("strdup");
-					free(file_name);
+				char           *token = strtok(NULL, " ");
+				if (!token) {
+					fprintf(stderr, "missing filename\n");
 					return 1;
 				}
-			}
-			file_writer(&fs, file_name, fs.movement_parent);
+				char           *file_name = strdup(token);
+				if (!file_name) {
+					perror("strdup");
+					return 1;
+				}
+				char           *folderpath = NULL;
+				token = strtok(NULL, " ");
+				if (token) {
+					folderpath = strdup(token);
+					if (!folderpath) {
+						perror("strdup");
+						free(file_name);
+						return 1;
+					}
+				}
+				file_writer(&fs, file_name, fs.movement_parent);
 
-			free(folderpath);
-			free(file_name);
-		} else if (strcmp(command, "addfolder") == 0) {
-			char           *folderpath = strtok(NULL, " ");
-			folder_file_handler(&fs, folderpath, fs.movement_parent);
-		} else if (strcmp(command, "extract") == 0) {
-			char           *item_name = strtok(NULL, " ");
-			fs_file_folder_handler(&fs, item_name, fs.movement_parent);
-		} else if (strcmp(command, "exit") == 0) {
-			loop = 0;
-		} else if (strcmp(command, "cd") == 0) {
-			char           *folder = strtok(NULL, " ");
-			if (strchr(folder, '/')) {
-				lfs_go_path(&fs, folder);
-			} else if (folder) {
-				lfs_cd(&fs, folder);
-			} else {
-				printf("undefined folder\n");
-			}
-		} else if (strcmp(command, "cat") == 0) {
-			char           *file = strtok(NULL, " ");
-			if (file) {
-				char           *out;
-				size_t 		size;
-				lfs_cat(&fs, file, fs.movement_parent, &out, &size);
-				printf("%s\nSIZE (bytes): %lu\n", out, size);
-				free(out);
-			} else {
-				printf("undefined file\n");
-			}
-		} else if (strcmp(command, "mkdir") == 0) {
-			char           *name = strtok(NULL, " ");
-			if (name) {
-				lfs_newdir(&fs, name, fs.movement_parent);
-			} else {
-				printf("undefined name\n");
-			}
-		} else if (strcmp(command, "new") == 0) {
-			char           *name = strtok(NULL, " ");
-			char           *datapart = strtok(NULL, " ");
-			if (name && datapart) {
-				char 		buffer   [4096] = "";
-				while (datapart) {
-					strcat(buffer, datapart);
-					strcat(buffer, " ");
-					datapart = strtok(NULL, " ");
+				free(folderpath);
+				free(file_name);
+			} else if (strcmp(command, "addfolder") == 0) {
+				char           *folderpath = strtok(NULL, " ");
+				folder_file_handler(&fs, folderpath, fs.movement_parent);
+			} else if (strcmp(command, "extract") == 0) {
+				char           *item_name = strtok(NULL, " ");
+				fs_file_folder_handler(&fs, item_name, fs.movement_parent);
+			} else if (strcmp(command, "exit") == 0) {
+				loop = 0;
+			} else if (strcmp(command, "cd") == 0) {
+				char           *folder = strtok(NULL, " ");
+				if (strchr(folder, '/')) {
+					lfs_go_path(&fs, folder);
+				} else if (folder) {
+					lfs_cd(&fs, folder);
+				} else {
+					printf("undefined folder\n");
 				}
-				lfs_newfile(&fs, name, buffer, strlen(buffer), fs.movement_parent);
-			} else {
-				printf("undefined file or data\n");
-			}
-		} else if (strcmp(command, "rmdir") == 0) {
-			char           *name = strtok(NULL, " ");
-			if (name) {
-				int 		offset = lfs_doffset(&fs, name, fs.movement_parent);
-				if (offset != -1) {
-					lfs_rmdir(&fs, name);
+			} else if (strcmp(command, "cat") == 0) {
+				char           *file = strtok(NULL, " ");
+				if (file) {
+					char           *out;
+					size_t 		size;
+					lfs_cat(&fs, file, fs.movement_parent, &out, &size);
+					printf("%s\nSIZE (bytes): %lu\n", out, size);
+					free(out);
+				} else {
+					printf("undefined file\n");
+				}
+			} else if (strcmp(command, "mkdir") == 0) {
+				char           *name = strtok(NULL, " ");
+				if (name) {
+					lfs_newdir(&fs, name, fs.movement_parent);
+				} else {
+					printf("undefined name\n");
+				}
+			} else if (strcmp(command, "new") == 0) {
+				char           *name = strtok(NULL, " ");
+				char           *datapart = strtok(NULL, " ");
+				if (name && datapart) {
+					char 		buffer   [4096] = "";
+					while (datapart) {
+						strcat(buffer, datapart);
+						strcat(buffer, " ");
+						datapart = strtok(NULL, " ");
+					}
+					lfs_newfile(&fs, name, buffer, strlen(buffer), fs.movement_parent);
+				} else {
+					printf("undefined file or data\n");
+				}
+			} else if (strcmp(command, "rmdir") == 0) {
+				char           *name = strtok(NULL, " ");
+				if (name) {
+					int 		offset = lfs_doffset(&fs, name, fs.movement_parent);
+					if (offset != -1) {
+						lfs_rmdir(&fs, name);
+					}
+				} else {
+					printf("undefined folder\n");
+				}
+			} else if (strcmp(command, "rm") == 0) {
+				char           *name = strtok(NULL, " ");
+				if (name) {
+					lfs_rm(&fs, name);
+				} else {
+					printf("undefined file\n");
 				}
 			} else {
-				printf("undefined folder\n");
+				printf("unrecognized command: %s\n", command);
 			}
-		} else if (strcmp(command, "rm") == 0) {
-			char           *name = strtok(NULL, " ");
-			if (name) {
-				lfs_rm(&fs, name);
-			} else {
-				printf("undefined file\n");
-			}
-		} else {
-			printf("unrecognized command: %s\n", command);
 		}
 	}
-	}
-	
 	if (nav != 1 && exr == 1) {
 		printf("extracting...\n");
 		cli_extracter(&fs, 0);
 	}
-	
 	if (nav != 1 && exr != 1 && cre == 1) {
 		printf("creating archive...\n");
 		if (!cre_folders) {
@@ -426,12 +417,12 @@ main(int argc, char **argv)
 			return 1;
 		}
 		if (strchr(cre_folders, ',')) {
-		char* folders = strtok(cre_folders, ",");
+			char           *folders = strtok(cre_folders, ",");
 
-		while (folders != NULL) {
-			folder_file_handler(&fs, folders, fs.movement_parent);
-			folders = strtok(NULL, ",");
-		}
+			while (folders != NULL) {
+				folder_file_handler(&fs, folders, fs.movement_parent);
+				folders = strtok(NULL, ",");
+			}
 		} else {
 			fs.movement_parent = 0;
 			folder_file_handler(&fs, cre_folders, fs.movement_parent);
@@ -439,31 +430,27 @@ main(int argc, char **argv)
 		free(cre_folders);
 
 		if (gzip == 1) {
-			int size = strlen("gzip ") + strlen(file) + 1;
-			char *full_command = malloc(size);
-			snprintf(full_command,  size, "gzip %s", file);
+			int 		size = strlen("gzip ") + strlen(file) + 1;
+			char           *full_command = malloc(size);
+			snprintf(full_command, size, "gzip %s", file);
 			system(full_command);
 			free(full_command);
-		} 
-		else if (xz == 1) {
-			int size = strlen("xz ") + strlen(file) + 1;
-			char *full_command = malloc(size);
-			snprintf(full_command,  size, "xz %s", file);
+		} else if (xz == 1) {
+			int 		size = strlen("xz ") + strlen(file) + 1;
+			char           *full_command = malloc(size);
+			snprintf(full_command, size, "xz %s", file);
 			system(full_command);
 			free(full_command);
 
-		} 
-		else if (lz4 == 1) {
-			int size = strlen("lz4 ") + strlen(file) + 1;
-			char *full_command = malloc(size);
-			snprintf(full_command,  size, "lz4 %s", file);
+		} else if (lz4 == 1) {
+			int 		size = strlen("lz4 ") + strlen(file) + 1;
+			char           *full_command = malloc(size);
+			snprintf(full_command, size, "lz4 %s", file);
 			system(full_command);
 			free(full_command);
 
 		}
-	
 	}
-
 	fclose(fs.img);
 	return 0;
 }
